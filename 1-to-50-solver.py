439c1ddfd09d5ea5c4e33e4720b5a2da055b4c6b
Laddicus <devonaladd@gmail.com>
2021-09-18 21:44:36 -0400

    some comments and refactoring

--- 1-to-50-solver.py
+++ 1-to-50-solver.py
@@ -1,31 +1,27 @@
 import pyautogui
 import threading
-REGION = (2535, 945, 370, 370)
-num = 1
-xPos = list()
-yPos = list()
 
-def search():
+# region of the screen that the number grid resides in
+REGION = (2535, 945, 370, 370)
+# used to keep track of the locations of each number
+position = list()
+for x in range(50):
+    position.append((0,0))
+
+# function for searching the screen for numbers
+def search(number):
     try:
-        global num
-        # pyautogui.locateOnScreen('./sc/1.png')
-        # print("test")
-        targetx, targety = pyautogui.center(pyautogui.locateOnScreen('./sc/%d.png' % num, grayscale = True, region=REGION))
-        xPos.append(targetx)
-        yPos.append(targety)
-        # if num >25:
-        #     print(num)
-        num += 1
+        position[number-1] = (pyautogui.center(pyautogui.locateOnScreen('./sc/%d.png' % number, grayscale = True, region=REGION)))
     except:
         print("Number not found")
         exit()
 def sFZ():
-    while num<=50:
-        search()
+    while filled(position)<50:
+        search(filled(position)+1)
 def clickTE(i):
     try:
-        if len(xPos) > i:
-            pyautogui.click(xPos[i], yPos[i])
+        if filled(position) > i:
+            pyautogui.click(position[i][0], position[i][1])
         else:
             pyautogui.sleep(0.001)
             clickTE(i)
@@ -37,13 +33,21 @@ def cFZ():
     for i in range(25, 50):
         clickTE(i)
 
+def filled(list):
+    for x, y in enumerate(list):
+            if y[0] == 0:
+                print(f"{x} : {y}")
+                return x
+    return len(list)
+
 t1 = threading.Thread(target=sFZ)
 t2 = threading.Thread(target=cFZ)
 
-while num<=25:
-    search()
+
+while filled(position)<25:
+    search(filled(position)+1)
 for i in range(25):
-    pyautogui.click(xPos[i], yPos[i])
+    pyautogui.click(position[i][0], position[i][1])
 
 pyautogui.moveTo(10,10)
 
